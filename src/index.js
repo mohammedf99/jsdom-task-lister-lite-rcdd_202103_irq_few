8aaad3efc53c64024ea371667f9ff842d47bfec8
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-04-11 20:12:27 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector('#create-task-form');
   const input = document.querySelector('#new-task-description');
+  const tasks = document.querySelector('#tasks');
   
   form.addEventListener('submit', function(){
     const li = document.createElement('li');
     li.innerText = input.value;
-    
+    tasks.append(li);
   })
 });
